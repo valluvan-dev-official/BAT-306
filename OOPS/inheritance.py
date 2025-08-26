bf83bdb43736fb11bc32f9ed7ea1176b6385dd05
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-08-26 12:53:34 +0530

    Add comprehensive examples and explanations for various inheritance types in Python

---
+++ OOPS/inheritance.py
@@ -0,0 +1,193 @@
+# Ineritance :
+
+
+# 1. Single Inheritance :
+
+"""
+    A child class inherits from one parent class.
+"""
+# Example :
+
+"""
+class Father: #Parent Class
+
+    name = "John"
+
+    def Car(self):
+
+        print("Father's Car")
+
+
+# Anbu = Father()
+
+# Anbu.Car()
+
+
+class Son(Father): # Child class
+
+    my_name = "David"
+
+    def Bike(self):
+
+        print("Son's Bike")
+
+
+
+Varun = Son()
+
+Varun.Bike()
+
+Varun.Car()
+
+print(Varun.name)
+
+"""
+
+# Multiple Inheritance :
+
+
+"""
+    A child class inherits from multiple parent classes.
+"""
+
+# Example :
+
+
+"""
+class Father: # Parent Class or Base Class
+
+    name = "John"
+
+    def Car(self):
+
+        print("Father's Car")
+
+
+class Mother: # Parent Class
+
+    name = "Jane"
+
+    def Van(self):
+
+        print("Mother's Van")
+
+
+class Son(Father, Mother): # Child class or Derived Class
+
+    my_name = "David"
+
+    def Bike(self):
+
+        print("Son's Bike")
+
+
+Varun = Son()
+
+Varun.Bike()
+
+Varun.Car()
+
+Varun.Van()
+
+print(Varun.name)
+
+"""
+
+# Multi-level Inheritance :
+
+"""
+
+- involve a chain of inheritance Where a child class becomes a parent for another class
+
+"""
+# Example :
+
+"""
+class Grandpa: # Parent Class
+
+    name = "Grandpa"
+
+    def House(self):
+
+        print("Grandpa's House")
+
+
+class Father(Grandpa): # Child class
+
+    name = "John"
+
+    def Car(self):
+
+        print("Father's Car")
+
+
+class Son(Father): # Child class
+
+    my_name = "David"
+
+    def Bike(self):
+
+        print("Son's Bike")
+
+
+Varun = Son()
+
+Varun.Bike()
+
+Varun.Car()
+
+Varun.House()
+
+print(Varun.name)
+
+"""
+
+# Hierarchical Inheritance :
+
+"""
+    Hierarchical Inheritance Occurs when multiple child classes inherit from a single parent class.
+"""
+# 
+class Father:
+
+    name = "John"
+
+    def Car(self):
+
+        print("Father's Car")
+
+
+
+class Son(Father):
+
+    my_name = "David"
+
+    def Bike(self):
+
+        print("Son's Bike")
+
+
+class Daughter(Father):
+
+    my_name = "Emma"
+
+    def Scooter(self):
+
+        print("Daughter's Scooter")
+
+
+
+Arun = Son()
+
+Arun.Bike()
+
+Arun.Car()
+
+
+Tina = Daughter()
+
+Tina.Scooter()
+
+Tina.Car()
+
+
